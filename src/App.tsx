26f949f52e2d52ca7ce493f9f815ef8138c900d2
ZachHunn <zach.hunn@trongov.us>
2024-05-15 17:41:50 -1000

    change page title

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
   return (
     <Container className='container'>
      
-      <Typography variant='h3' sx={{marginY: 2, textAlign: 'center'}}>Chief Thompson's Recommendations</Typography>
+      <Typography variant='h3' sx={{marginY: 2, textAlign: 'center'}}>15th Wing Command Chief's Recommendations</Typography>
       <Typography variant='h4' sx={{textAlign: 'center'}}>"Success leaves clues"</Typography>
       <Typography variant='h5' sx={{textAlign: 'center', marginBottom: 3}}>- Jim Kwik</Typography>
       <Typography variant='h4' sx={{textAlign: 'center', textDecoration: 'underline'}}>Books</Typography>
